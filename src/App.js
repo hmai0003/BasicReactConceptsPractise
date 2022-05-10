e3d14fe6d4e0832b2ee15b011430fd283fe18da2
hmai0003 <harsh_mairh@yahoo.co.in>
2022-05-10 23:41:44 +0930

    Logic to add users to the list & added Error Modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import  AddUsers  from './components/Users/AddUsers'
+import React, {useState} from 'react';
+import  AddUsers  from './components/Users/AddUsers';
+import UsersList from './components/Users/UsersList';
 
 
 function App() {
+  const [usersList, updateUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    updateUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}]
+    });
+  }
+
   return (
     <div>
-      <AddUsers />
+      <AddUsers onAddUser={addUserHandler}/>
+      { usersList.length > 0 && <UsersList users={usersList}></UsersList>}
     </div>
   );
 }
